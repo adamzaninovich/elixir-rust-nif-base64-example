9c8faaba7c884b0de135b3727e75e3026348fd55
Adam Zaninovich <396237+adamzaninovich@users.noreply.github.com>
2022-08-06 09:31:24 -0700

    better variable names

--- lib/base64.ex
+++ lib/base64.ex
@@ -2,10 +2,10 @@ defmodule Base64 do
   use Rustler, otp_app: :base64, crate: "base64_nif"
 
   @spec decode(binary, atom) :: binary
-  def decode(_b64, _opt \\ :standard), do: error()
+  def decode(_base64, _opt \\ :standard), do: error()
 
   @spec encode(binary, atom) :: binary
-  def encode(_s, _opt \\ :standard), do: error()
+  def encode(_string, _opt \\ :standard), do: error()
 
   defp error, do: :erlang.nif_error(:nif_not_loaded)
 end
